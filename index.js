b87d7bcedd55106eb04f62e44f63c26a1f6fcc64
Mason Phillips <math.matrix@icloud.com>
2018-04-22 01:57:04 -0500

    Updates to site, adding links, removing docs folder and preparing PM2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const data = require('./data');
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -7,7 +8,19 @@ app.set('views', __dirname + '/views');
 app.use('/assets', express.static(__dirname + '/resources'));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {dd: JSON.stringify(data)});
 });
+
+app.get('/professional', function(req, res) {
+    res.render('prof', {dd: JSON.stringify(data)});
+});
+
+app.get('/social', function(req, res) {
+    res.render('social', {dd: JSON.stringify(data)});
+});
+
+app.get('/download/resume', function(req, res) {
+    res.download(__dirname + '/resources/downloads/Resume 2018.docx', 'Resume - Mason Phillips.docx');
+})
 
 app.listen(8080, () => console.log('Running on 8080'));

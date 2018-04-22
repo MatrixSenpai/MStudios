484bd14d24a24fc8f5218f11c92d7c8501890b62
Mason Phillips <math.matrix@icloud.com>
2018-04-22 02:52:51 -0500

    Adding analytics

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
+const ua = require('universal-analytics');
+
 const data = require('./data');
+
+const app = express();
+const v = ua('UA-117987097-1');
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -8,17 +12,22 @@ app.set('views', __dirname + '/views');
 app.use('/assets', express.static(__dirname + '/resources'));
 
 app.get('/', function(req, res) {
-    res.render('index', {dd: JSON.stringify(data)});
+    v.pageview('/', 'Home').send();
+    res.render('index', {dd: JSON.stringify(data), active: '.home'});
 });
 app.get('/professional', function(req, res) {
-    res.render('prof', {dd: JSON.stringify(data)});
+    v.pageview('/professional', 'Portfolio Page').send();
+    res.render('prof', {dd: JSON.stringify(data), active: '.professional'});
 });
 app.get('/social', function(req, res) {
-    res.render('social', {dd: JSON.stringify(data)});
+    v.pageview('/social', 'Social Page').send();
+    res.render('social', {dd: JSON.stringify(data), active: '.social'});
 });
 
 app.get('/media/:link', function(req, res) {
     var social = req.params.link;
+
+    v.pageview('/media/' + social, social).send();
 
     res.redirect(data.links[social]);
 });
